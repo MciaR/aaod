0f2e2d405480da5ee184814808e433ce6b0f7312
Xuefei Niu <nnsmile526@gmail.com>
2023-06-01 12:14:07 +0800

    add gt,dt draw

--- tools/vis_featmap.py
+++ tools/vis_featmap.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 from utils import AAVisualizer
 from PIL import Image
@@ -14,6 +15,10 @@ class ExpVisualizer():
     def __init__(self, cfg_file, ckpt_file):
         self.visualizer = AAVisualizer(cfg_file=cfg_file, ckpt_file=ckpt_file)
         self.dataset = self.visualizer.get_dataset()
+    
+    @staticmethod
+    def get_timestamp():
+        return time.strftime("%Y%m%dT%H%M%S", time.gmtime())
 
     def show_single_pic_feats(self, img, show_layer=0, top_k = 100, pic_overlay=False):
         """Show `top_k` channels of featuremap of a pic."""
@@ -33,11 +38,34 @@ class ExpVisualizer():
         heatmap = self.visualizer.draw_featmap(_feature, _image, channel_reduction=None, arrangement=(10, 10), topk=top_k, with_text=False, alpha=0.5, resize_shape=_resize_shape)
         self.visualizer.show(img=heatmap)
 
-    def show_cmp_results(self, img):
-        """Show `ori_img`, `squeeze_mean_channel(all output stages)`, `upsample and overlay heatmap img(all output stages)`."""
-        img_path = img
+    def show_cmp_results(
+            self, 
+            img=None,
+            data_samples=None,
+            save=False,
+            stage='backbone'):
+        """Show `ori_img`, `squeeze_mean_channel(all output stages)`, `upsample and overlay heatmap img(all output stages)`.
+        Args:
+            img (str): path of img.
+            data_samples (DetDataSample): e.g. dataset[0]['data_sample'].
+            save (bool): whether save pic. if it is True, pic will not be shown when running.
+            model (str): string and model map. e.g. `'backbone'` - `model.backbone`, `'fpn'` - `model.fpn`.
+
+        """
+        assert img is not None or data_samples is not None, \
+            f'`img` and `data_samples` cannot be None both.'
+        if data_samples is None:
+            img_path = img
+        else:
+            img_path = data_samples.img_path
+        
 
         feat = self.visualizer._forward(self.visualizer.model.backbone, img=img_path)
+        if stage == 'neck':
+            feat = self.visualizer.model.neck(feat)
+        elif stage == 'head':
+            feat = self.visualizer.model.neck(feat)
+            feat = self.visualizer.model.head(feat)
         output_stages = len(feat)  # channels : (256, 512, 1024, 2048) and indices: [0, 1, 2, 3] for fpn
 
         image = Image.open(img_path)
@@ -47,10 +75,17 @@ class ExpVisualizer():
 
         _resize_shape = (_image.shape[0], _image.shape[1])
 
-        plt.figure(frameon=False, figsize=(15, 12))
+        plt.figure(frameon=False, figsize=(10, 8))
         plt.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0, hspace=0)
 
         ind = 1
+        gt_image = self.visualizer.draw_dt_gt(
+            name='gt',
+            image=_image,
+            data_sample=data_samples,
+            draw_gt=True,
+            draw_pred=False
+        )
         # ====== First row: original pic ======
         for i in range(col):
             plt.subplot(row, col, ind)
@@ -58,7 +93,7 @@ class ExpVisualizer():
             plt.yticks([],[])
             if i == 0:
                 plt.ylabel(f"Origin Pic")
-            plt.imshow(_image)
+            plt.imshow(gt_image)
             plt.title(f"({_image.shape[0]} x {_image.shape[1]})", fontsize=10)
             ind += 1
         
@@ -77,7 +112,7 @@ class ExpVisualizer():
 
         # ====== Third row: upsample and overlay heatmap img ======
         for i in range(col):
-            ax = plt.subplot(row, col, ind)
+            plt.subplot(row, col, ind)
             plt.xticks([],[])
             plt.yticks([],[])
             if i == 0:
@@ -88,6 +123,7 @@ class ExpVisualizer():
             result_heatmap = self.visualizer.draw_dt_gt(
                 name='result',
                 image=heatmap,
+                draw_gt=False,
                 data_sample=result
             )
             plt.title(f"({_image.shape[0]} x {_image.shape[1]})", fontsize=10)
@@ -95,7 +131,11 @@ class ExpVisualizer():
             ind += 1
         
         plt.tight_layout()
-        plt.show()
+        if save:
+            img_name = img_path.split('/')[-1].split('.')[0]
+            plt.savefig('records/pics/featmap/{}_{}_{}.png'.format(self.get_timestamp(), img_name, stage))
+        else:
+            plt.show()
 
 
 if __name__ == '__main__':
@@ -110,7 +150,9 @@ if __name__ == '__main__':
     pic_overlay = False
 
     # vis.show_single_pic_feats(img=img, show_layer=3, top_k=top_k, pic_overlay=pic_overlay)
-    vis.show_cmp_results(img=img)
+    dataset = vis.dataset
+    vis.show_cmp_results(data_samples=dataset[0]['data_samples'], stage='neck', save=True)
+    # vis.show_cmp_results(img=img)
     # dataset = vis.dataset
     # for data in dataset:
     #     print(data)
--- utils/aa_visualizer.py
+++ utils/aa_visualizer.py
@@ -1,7 +1,10 @@
 from typing import Dict, List, Optional, Tuple, Union
 
+import cv2
 import numpy as np
 import torch
+import torch.nn.functional as F
+import warnings
 
 from mmcv.transforms import Compose
 from mmdet.registry import DATASETS
@@ -9,6 +12,7 @@ from mmdet.utils import get_test_pipeline_cfg
 from mmdet.apis import init_detector, inference_detector
 from mmdet.visualization import DetLocalVisualizer
 from mmdet.structures import DetDataSample
+from mmengine.visualization.utils import img_from_canvas
 
 
 class AAVisualizer(DetLocalVisualizer):
@@ -102,6 +106,9 @@ class AAVisualizer(DetLocalVisualizer):
 
         if data_sample is not None:
             data_sample = data_sample.cpu()
+        
+        # change type of bboxes to Tensor or Numpy
+        data_sample.gt_instances.bboxes = data_sample.gt_instances.bboxes.numpy()
 
         if draw_gt and data_sample is not None:
             gt_img_data = image
@@ -161,3 +168,188 @@ class AAVisualizer(DetLocalVisualizer):
             feat = model(input_data)
 
         return feat
+
+    @staticmethod
+    def convert_overlay_heatmap(feat_map: Union[np.ndarray, torch.Tensor],
+                                img: Optional[np.ndarray] = None,
+                                alpha: float = 0.5,
+                                grey: bool = False) -> np.ndarray:
+        """Convert feat_map to heatmap and overlay on image, if image is not None.
+
+        Args:
+            feat_map (np.ndarray, torch.Tensor): The feat_map to convert
+                with of shape (H, W), where H is the image height and W is
+                the image width.
+            img (np.ndarray, optional): The origin image. The format
+                should be RGB. Defaults to None.
+            alpha (float): The transparency of featmap. Defaults to 0.5.
+
+        Returns:
+            np.ndarray: heatmap
+        """
+        assert feat_map.ndim == 2 or (feat_map.ndim == 3
+                                    and feat_map.shape[0] in [1, 3])
+        if isinstance(feat_map, torch.Tensor):
+            feat_map = feat_map.detach().cpu().numpy()
+
+        if feat_map.ndim == 3:
+            feat_map = feat_map.transpose(1, 2, 0)
+
+        norm_img = np.zeros(feat_map.shape)
+        norm_img = cv2.normalize(feat_map, norm_img, 0, 255, cv2.NORM_MINMAX)
+        norm_img = np.asarray(norm_img, dtype=np.uint8)
+        if grey:
+            heat_img = norm_img
+        heat_img = cv2.applyColorMap(norm_img, cv2.COLORMAP_JET)
+        heat_img = cv2.cvtColor(heat_img, cv2.COLOR_BGR2RGB)
+        if img is not None:
+            heat_img = cv2.addWeighted(img, 1 - alpha, heat_img, alpha, 0)
+        return heat_img
+    
+    def draw_featmap(self,
+                     featmap: torch.Tensor,
+                     overlaid_image: Optional[np.ndarray] = None,
+                     channel_reduction: Optional[str] = 'squeeze_mean',
+                     topk: int = 20,
+                     arrangement: Tuple[int, int] = (4, 5),
+                     resize_shape: Optional[tuple] = None,
+                     grey: bool = False,
+                     alpha: float = 0.5) -> np.ndarray:
+        """Draw featmap.
+
+        - If `overlaid_image` is not None, the final output image will be the
+          weighted sum of img and featmap.
+
+        - If `resize_shape` is specified, `featmap` and `overlaid_image`
+          are interpolated.
+
+        - If `resize_shape` is None and `overlaid_image` is not None,
+          the feature map will be interpolated to the spatial size of the image
+          in the case where the spatial dimensions of `overlaid_image` and
+          `featmap` are different.
+
+        - If `channel_reduction` is "squeeze_mean" and "select_max",
+          it will compress featmap to single channel image and weighted
+          sum to `overlaid_image`.
+
+        - If `channel_reduction` is None
+
+          - If topk <= 0, featmap is assert to be one or three
+            channel and treated as image and will be weighted sum
+            to ``overlaid_image``.
+          - If topk > 0, it will select topk channel to show by the sum of
+            each channel. At the same time, you can specify the `arrangement`
+            to set the window layout.
+
+        Args:
+            featmap (torch.Tensor): The featmap to draw which format is
+                (C, H, W).
+            overlaid_image (np.ndarray, optional): The overlaid image.
+                Defaults to None.
+            channel_reduction (str, optional): Reduce multiple channels to a
+                single channel. The optional value is 'squeeze_mean'
+                or 'select_max'. Defaults to 'squeeze_mean'.
+            topk (int): If channel_reduction is not None and topk > 0,
+                it will select topk channel to show by the sum of each channel.
+                if topk <= 0, tensor_chw is assert to be one or three.
+                Defaults to 20.
+            arrangement (Tuple[int, int]): The arrangement of featmap when
+                channel_reduction is not None and topk > 0. Defaults to (4, 5).
+            resize_shape (tuple, optional): The shape to scale the feature map.
+                Defaults to None.
+            alpha (Union[int, List[int]]): The transparency of featmap.
+                Defaults to 0.5.
+
+        Returns:
+            np.ndarray: RGB image.
+        """
+        import matplotlib.pyplot as plt
+        assert isinstance(featmap,
+                          torch.Tensor), (f'`featmap` should be torch.Tensor,'
+                                          f' but got {type(featmap)}')
+        assert featmap.ndim == 3, f'Input dimension must be 3, ' \
+                                  f'but got {featmap.ndim}'
+        featmap = featmap.detach().cpu()
+
+        if overlaid_image is not None:
+            if overlaid_image.ndim == 2:
+                overlaid_image = cv2.cvtColor(overlaid_image,
+                                              cv2.COLOR_GRAY2RGB)
+
+            if overlaid_image.shape[:2] != featmap.shape[1:]:
+                warnings.warn(
+                    f'Since the spatial dimensions of '
+                    f'overlaid_image: {overlaid_image.shape[:2]} and '
+                    f'featmap: {featmap.shape[1:]} are not same, '
+                    f'the feature map will be interpolated. '
+                    f'This may cause mismatch problems !')
+                if resize_shape is None:
+                    featmap = F.interpolate(
+                        featmap[None],
+                        overlaid_image.shape[:2],
+                        mode='bilinear',
+                        align_corners=False)[0]
+
+        if resize_shape is not None:
+            featmap = F.interpolate(
+                featmap[None],
+                resize_shape,
+                mode='bilinear',
+                align_corners=False)[0]
+            if overlaid_image is not None:
+                overlaid_image = cv2.resize(overlaid_image, resize_shape[::-1])
+
+        if channel_reduction is not None:
+            assert channel_reduction in [
+                'squeeze_mean', 'select_max'], \
+                f'Mode only support "squeeze_mean", "select_max", ' \
+                f'but got {channel_reduction}'
+            if channel_reduction == 'select_max':
+                sum_channel_featmap = torch.sum(featmap, dim=(1, 2))
+                _, indices = torch.topk(sum_channel_featmap, 1)
+                feat_map = featmap[indices]
+            else:
+                feat_map = torch.mean(featmap, dim=0)
+            
+            return self.convert_overlay_heatmap(feat_map, overlaid_image, alpha, grey)
+        elif topk <= 0:
+            featmap_channel = featmap.shape[0]
+            assert featmap_channel in [
+                1, 3
+            ], ('The input tensor channel dimension must be 1 or 3 '
+                'when topk is less than 1, but the channel '
+                f'dimension you input is {featmap_channel}, you can use the'
+                ' channel_reduction parameter or set topk greater than '
+                '0 to solve the error')
+            return self.convert_overlay_heatmap(featmap, overlaid_image, alpha, grey)
+        else:
+            row, col = arrangement
+            channel, height, width = featmap.shape
+            assert row * col >= topk, 'The product of row and col in ' \
+                                      'the `arrangement` is less than ' \
+                                      'topk, please set the ' \
+                                      '`arrangement` correctly'
+
+            # Extract the feature map of topk
+            topk = min(channel, topk)
+            sum_channel_featmap = torch.sum(featmap, dim=(1, 2))
+            _, indices = torch.topk(sum_channel_featmap, topk)
+            topk_featmap = featmap[indices]
+
+            fig = plt.figure(frameon=False)
+            # Set the window layout
+            fig.subplots_adjust(
+                left=0, right=1, bottom=0, top=1, wspace=0, hspace=0)
+            dpi = fig.get_dpi()
+            fig.set_size_inches((width * col + 1e-2) / dpi,
+                                (height * row + 1e-2) / dpi)
+            for i in range(topk):
+                axes = fig.add_subplot(row, col, i + 1)
+                axes.axis('off')
+                axes.text(2, 15, f'channel: {indices[i]}', fontsize=10)
+                axes.imshow(
+                    self.convert_overlay_heatmap(topk_featmap[i], overlaid_image,
+                                            alpha, grey))
+            image = img_from_canvas(fig.canvas)
+            plt.close(fig)
+            return image
