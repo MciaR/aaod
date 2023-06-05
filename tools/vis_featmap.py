b47b48896ebc5339ae7ac5fa984d59b9e89fb313
Xuefei Niu <nnsmile526@gmail.com>
2023-06-05 11:51:50 +0800

    add ssd300 voc and coco config

--- tools/vis_featmap.py
+++ tools/vis_featmap.py
@@ -7,9 +7,8 @@ def parse_args():
 
 if __name__ == '__main__':
     # 指定模型的配置文件和 checkpoint 文件路径
-    config_file = 'configs/faster_rcnn_r101_fpn.py'
-    checkpoint_file = 'pretrained/resnet/faster_rcnn_r101_fpn_1x_coco_20200130-f513f705.pth'
-    img = 'data/coco2014/images/val2014/COCO_val2014_000000000042.jpg'
+    config_file = 'configs/faster_rcnn_r101_fpn_voc.py'
+    checkpoint_file = '.work_dir/fr_r101_voc/epoch_9_map07699.pth'
 
     vis = ExpVisualizer(cfg_file=config_file, ckpt_file=checkpoint_file, use_attack=True)
     show_layer = 3 # (256, 512, 1024, 2048) channels
@@ -18,7 +17,7 @@ if __name__ == '__main__':
 
     # vis.show_single_pic_feats(img=img, show_layer=3, top_k=top_k, pic_overlay=pic_overlay)
     dataset = vis.dataset
-    vis.show_cmp_results(data_samples=dataset[0]['data_samples'], stage='backbone', save=True, grey=False)
+    vis.show_cmp_results(data_samples=dataset[0]['data_samples'], stage='backbone', save=False, grey=False)
     # vis.show_cmp_results(data_samples=dataset[0]['data_samples'], stage='backbone', save=True, grey=True)
     # vis.show_cmp_results(img=img)
     # dataset = vis.dataset
