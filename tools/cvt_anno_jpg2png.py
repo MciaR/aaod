ab0b5782b76c0a5dca7514e160b36dfa514196c8
Xuefei Niu <nnsmile526@gmail.com>
2024-01-09 15:19:01 +0800

    update voc_cvt_anno_jpg2png func

--- tools/cvt_anno_jpg2png.py
+++ tools/cvt_anno_jpg2png.py
@@ -1,8 +1,10 @@
 import json
 import os
 import pandas as pd
+import xml.etree.ElementTree as ET
+from tqdm import tqdm
 
-def cvt_anno_jpg2png(anno_path, anno_file_name):
+def cvt_anno_jpg2png_coco(anno_path, anno_file_name):
     anno_file = os.path.join(anno_path, anno_file_name)
     with open(anno_file, "r") as f:
         anno = json.load(f)
@@ -17,9 +19,38 @@ def cvt_anno_jpg2png(anno_path, anno_file_name):
     with open(save_dir, "w") as f:
         f.write(anno_json)
 
+def cvt_anno_jpg2png_voc(anno_path, anno_target_path):
+    if not os.path.exists(anno_target_path):
+        os.makedirs(anno_target_path)
+
+    for filename in tqdm(os.listdir(anno_path)):
+        if filename.endswith('.xml'):
+            # 构建完整的文件路径
+            filepath = os.path.join(anno_path, filename)
+
+            # 解析 XML 文件
+            tree = ET.parse(filepath)
+            root = tree.getroot()
+
+            # 查找并更新 filename 标签
+            filename_tag = root.find('filename')
+            if filename_tag is not None:
+                filename_without_ext = os.path.splitext(filename_tag.text)[0]
+                filename_tag.text = filename_without_ext + '.png'
+
+            # 保存修改后的 XML 文件
+            target_file_path = os.path.join(anno_target_path, filename)
+            tree.write(target_file_path)
+    
+    print("Convert has done!")
+    
 
 if __name__ == '__main__':
-    anno_path = 'data/coco2017/annotations'
-    anno_file_name = 'tiny_val2017.json'
-    cvt_anno_jpg2png(anno_path, anno_file_name)
+    # anno_path = 'data/coco2017/annotations'
+    # anno_file_name = 'tiny_val2017.json'
+    # cvt_anno_jpg2png_coco(anno_path, anno_file_name)
+
+    anno_path = 'data/VOCdevkit/tiny_voc/Annotations'
+    anno_target_path = 'data/VOCdevkit/tiny_voc/Annotations_png'
+    cvt_anno_jpg2png_voc(anno_path, anno_target_path)
     
