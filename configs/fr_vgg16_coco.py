c0ad6d26c2b3813196f0511ab007a7c88aef94e2
Sheleph.niu <nnsmile526@gmail.com>
2024-01-12 10:18:05 +0800

    update fr_vgg16_coco batch_size to 2

--- configs/fr_vgg16_coco.py
+++ configs/fr_vgg16_coco.py
@@ -1,4 +1,1 @@
 _base_ = ['./base/models/fr_vgg16.py', './base/datasets/coco_datasets.py', './base/default_runtime.py', './base/schedules/schedule_1x.py']
-
-train_dataloader = dict(
-    batch_size=4)
